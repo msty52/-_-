d82a1103f7d47d6ae7d7e02afb122d8812fcc615
msty52 <89525bobbob@gmail.com>
2026-04-17 16:33:56 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,29 @@
 import json
+from js import Response, fetch
 
 async def on_fetch(request, env):
     if request.method == "POST":
         try:
-            # Читаем сообщение от Telegram
             data = await request.json()
-            chat_id = data['message']['chat']['id']
-            text = data['message'].get('text', '')
+            if "message" in data:
+                chat_id = data['message']['chat']['id']
+                text = data['message'].get('text', 'Без текста')
 
-            # Отправляем ответ напрямую через API Telegram
-            url = f"https://api.telegram.org/bot{env.BOT_TOKEN}/sendMessage"
-            payload = {
-                "chat_id": chat_id,
-                "text": f"Бот на Cloudflare работает! Вы написали: {text}"
-            }
+                token = "8783400059:AAGKuUHfxkE4mMKlTGMKsVmgQcGjGQ4CNdk"
+                url = f"https://api.telegram.org/bot{token}/sendMessage"
+                
+                payload = {
+                    "chat_id": chat_id,
+                    "text": f"Бот на Cloudflare работает! Ты написал: {text}"
+                }
+
+                # Отправка через встроенный fetch
+                await fetch(url, method="POST", 
+                            body=json.dumps(payload), 
+                            headers={"Content-Type": "application/json"})
             
-            # Делаем запрос к Telegram
-            await fetch(url, method="POST", body=json.dumps(payload), headers={"Content-Type": "application/json"})
-            
-            return Response.new("ok", status=200)
+            return Response.new("ok")
         except Exception as e:
-            return Response.new(str(e), status=200)
+            return Response.new(str(e))
 
-    return Response.new("Бот онлайн и ждет сообщений!", status=200)
+    return Response.new("Бот успешно запущен!")
